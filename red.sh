16df645628795422182b3822fccbdd9fb8234828
Yashukhin Artem <yashart@mail.ru>
2017-12-01 15:04:00 +0300

    add backup

--- red.sh
+++ red.sh
@@ -1,1 +1,1 @@
-g++ red_detection.cpp -I/usr/include -L/usr/lib -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_video
+g++ red_detection.cpp -std=c++11 -I/usr/include -L/usr/lib -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_video
